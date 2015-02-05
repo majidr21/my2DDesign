cf8806a0da3f3535fbac3eb9aecc3eef6b213c9d
majidr21 <majidr4@yahoo.com>
2015-02-05 09:33:20 -0700

    Created an object

---
+++ src/my2DDesign/aircraft.java
@@ -0,0 +1,21 @@
+package my2DDesign;
+
+public class aircraft
+{
+	public static void main(String [] args) {
+		int var1 = 5;
+		double var2 = 3.65;
+		
+		aircraft cessna172 = new aircraft();		//creates object of class aircraft
+		
+		aircraft piperSaratoga = new aircraft();
+		
+	}
+
+
+	int passengers;			// number of people
+	int cruiseSpeed;		// miles per hour
+	double fuelCapacity;	// gallons
+	double fuelBurnRate;	// gallons per hour
+	
+}
